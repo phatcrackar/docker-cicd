9e86f47bb5d86a57c341e43177283bda81da3179
phatcrackar <roni.karpman.rkk@gmail.com>
2019-08-07 13:30:09 +0300

    boilerplate for pipeline for dsl

--- basics/jenkins/job-dsl/nodejs.groovy
+++ basics/jenkins/job-dsl/nodejs.groovy
@@ -43,3 +43,31 @@ job('NodeJS Docker example') {
         }
     }
 }
+
+
+job('boilerplate-pipeline') {
+    scm {
+        git('git://github.com/phatcrackar/docker-cicd.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs_demo') 
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('phatcrackar/jenkins-demo') //qa / dev
+            tag('${GIT_REVISION,length=9}')
+            buildContext('./basics')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
